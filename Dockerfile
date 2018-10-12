7fbd7cc4d81a916a1dc04c64c62e415ea99216b5
evi0s <1417213864@qq.com>
2018-10-13 04:18:03 +0800

    Fix bugs

--- Dockerfile
+++ Dockerfile
@@ -11,5 +11,8 @@ ADD . /app
 
 WORKDIR /app
 
+# Executable
+RUN chmod +x /app/deploy/entrypoint.sh
+
 # EntryPoint
 ENTRYPOINT /app/deploy/entrypoint.sh
--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -8,31 +8,31 @@ npm install
 
 if [ ! -z "$DBHOST" ]; then
   sed -i "8d" $APP/config.json
-  sed -i '7a \t"database_host" : "$DBHOST",' $APP/config.json
+  sed -i '7a  "database_host" : "'"$DBHOST"'",' $APP/config.json
   sed -i "9d" $APP/node_modules/stdmgr/lib/usrmgr.js
-  sed -i '8a const url = "mongodb://$DBHOST:27017";' $APP/node_modules/stdmgr/lib/usrmgr.js
+  sed -i '8aconst url = "mongodb://'"$DBHOST"':27017";' $APP/node_modules/stdmgr/lib/usrmgr.js
   sed -i "9d" $APP/node_modules/tchmgr/lib/tchmgr.js
-  sed -i '8a const url = "mongodb://$DBHOST:27017";' $APP/node_modules/tchmgr/lib/tchmgr.js
+  sed -i '8aconst url = "mongodb://'"$DBHOST"':27017";' $APP/node_modules/tchmgr/lib/tchmgr.js
 fi
 
 if [ ! -z "$DBNAME" ]; then
   sed -i "9d" $APP/config.json
-  sed -i '8a \t"database" : "$DBNAME",' $APP/config.json
+  sed -i '8a  "database" : "'"$DBNAME"'"' $APP/config.json
 fi
 
 if [ ! -z "$REDISHOST" ]; then
   sed -i "5d" $APP/config.json
-  sed -i '4a \t"redis_host" : "$REDISHOST",' $APP/config.json
+  sed -i '4a  "redis_host" : "'"$REDISHOST"'",' $APP/config.json
 fi
 
 if [ ! -z "$LISTENADDR" ]; then
   sed -i "3d" $APP/config.json
-  sed -i '2a \t"listen" : "$LISTENADDR",' $APP/config.json
+  sed -i '2a  "listen" : "'"$LISTENADDR"'",' $APP/config.json
 fi
 
 if [ ! -z "$LISTENPORT" ]; then
   sed -i "4d" $APP/config.json
-  sed -i '3a \t"port" : $LISTENPORT,' $APP/config.json
+  sed -i '3a  "port" : '"$LISTENPORT"',' $APP/config.json
 fi
 
 exec npm start
